77645256bd9fa6194d6b4ad620a3d86c41c9382a
rifalghurofan <rifalghurofan@gmail.com>
2022-08-30 14:49:40 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const cors = require ('cors');
 const app = express();
-const {db} = require('./model/dbConnection');
+const {db} = require('./dbConnection');
 
 app.use(cors());
 app.use(express.json());
